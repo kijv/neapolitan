f0a0e3776ae5c2ef2fb09f98e4580915da0b2cfe
kijv <93486795+kijv@users.noreply.github.com>
2025-08-17 15:08:01 -0400

    next plugin: webpack support

--- packages/neapolitan/src/next/index.ts
+++ packages/neapolitan/src/next/index.ts
@@ -1,9 +1,10 @@
 import '../runtime.d.ts'
 
+import type { Configuration, Resolver } from 'webpack'
+import { NEAPOLITAN_CTX_ID, NEAPOLITAN_INPUT_ID } from '../loaderutils.ts'
+import type { NeapolitanConfig } from '../config'
 import type { NextConfig } from 'next'
 import { fileURLToPath } from 'node:url'
-import type { NeapolitanConfig } from '../config'
-import { NEAPOLITAN_CTX_ID, NEAPOLITAN_INPUT_ID } from '../loaderutils.ts'
 
 export const defineConfig = (options: NeapolitanConfig): NeapolitanConfig =>
   options
@@ -17,38 +18,96 @@ export interface NeapolitanNextPluginOptions {
 export const createNeapolitan = (
   options: NeapolitanNextPluginOptions = {}
 ): ((nextConfig?: NextConfig) => NextConfig) => {
-  return <const T extends NextConfig>(nextConfig?: T) => {
+  return <const T extends NextConfig>(nextConfig: T = {} as T) => {
     const loaderOptions = {
       configPath: options.configPath,
     } as Record<string, string>
 
+    const loadAndTransformLoaders = [
+      {
+        loader: resolve('neapolitan/next/loader/load'),
+        options: loaderOptions,
+      },
+      {
+        loader: resolve('neapolitan/next/loader/transform'),
+        options: loaderOptions,
+      },
+    ]
+    const neapolitanLoader = {
+      loader: resolve('neapolitan/next/loader/neapolitan'),
+      options: loaderOptions,
+    }
+
+    const resolveAlias = {
+      [NEAPOLITAN_CTX_ID]: `neapolitan/next?ctx`,
+      [NEAPOLITAN_INPUT_ID]: `neapolitan/next?input`,
+    }
+
     return Object.assign({}, nextConfig ?? {}, {
+      webpack(config: Configuration, context) {
+        config.resolve ||= {}
+
+        config.module ||= {}
+        config.module.rules ||= []
+
+        config.module.rules.unshift({
+          test: () => true,
+          use: loadAndTransformLoaders,
+        })
+
+        config.module.rules.push({
+          test: (value) => Object.values(resolveAlias).map(resolve).includes(value),
+          use: [context.defaultLoaders.babel, neapolitanLoader],
+        })
+
+        config.plugins ||= []
+
+        config.resolve.plugins ||= []
+        config.resolve.plugins.push({
+          apply(resolver: Resolver) {
+            const target = resolver.ensureHook('resolve')
+
+            resolver
+              .getHook('resolve')
+              .tapAsync(
+                'neapolitan',
+                async (request, resolveContext, callback) => {
+                  if (
+                    !request.request ||
+                    !Object.keys(resolveAlias).includes(request.request)
+                  )
+                    return callback()
+
+                  // construct the new request
+                  const newRequest = {
+                    ...request,
+                    request: resolveAlias[request.request as keyof typeof resolveAlias],
+                  }
+
+                  // redirect the resolver
+                  resolver.doResolve(
+                    target,
+                    newRequest,
+                    null,
+                    resolveContext,
+                    callback
+                  )
+                }
+              )
+          },
+        })
+
+        return nextConfig.webpack?.(config, context) ?? config
+      },
       turbopack: {
-        resolveAlias: {
-          [NEAPOLITAN_CTX_ID]: `neapolitan/next?ctx`,
-          [NEAPOLITAN_INPUT_ID]: `neapolitan/next?input`,
-        },
+        resolveAlias,
         rules: {
           '*': {
-            loaders: [
-              {
-                loader: resolve('neapolitan/next/loader/load'),
-                options: loaderOptions,
-              },
-              {
-                loader: resolve('neapolitan/next/loader/transform'),
-                options: loaderOptions,
-              },
-            ],
+            loaders: loadAndTransformLoaders,
             as: '*',
           },
           '*next.js*': {
-            loaders: [
-              {
-                loader: resolve('neapolitan/next/loader/neapolitan'),
-                options: loaderOptions,
-              },
-            ],
+            loaders: [neapolitanLoader],
             as: '*.js',
           },
         },
